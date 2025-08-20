f7f198753fcf4093b851879ea8e9ae820159d5ec
Mohammed Farhaan Buckas <mohammedfarhaanbuckas@outlook.com>
2025-08-20 16:26:58 +0200

    Fetch login via API base and clarify splash

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/status': 'http://localhost:8001',
+      '/api': 'http://localhost:8001'
+    }
+  }
 })
